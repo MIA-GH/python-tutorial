ac40f9aa5f462082bbb2ed491c3555dc3ba06cf4
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2020-11-29 20:54:25 +0000

    initial commit

---
+++ source/intro.py
@@ -0,0 +1,80 @@
+# display output to the user
+# print("Hello world!")
+
+'''
+This package is built for ------
+'''
+
+# variables
+'''
+if, else, break, continue... # these cant be variables names
+_variablename
+VariableName
+variableName
+'''
+name = "Haqq"
+age = 56 
+distance = 123.56
+
+camelCase = "I am the camel case variable\n"
+
+# print(name)
+
+# # # python data types
+# float, int, double, string, [arrays, collection,] (linked list, stack, deque)
+
+# # number and operations
+# (+ - * / // %)
+
+x, y = 2, 5 
+add = x + y 
+sub = y - x
+flt_div = y / x
+int_div = y // x
+mul = y * x
+mod = y % x
+
+# print("the result is", flt_div, int_div)
+
+# commenting
+# this is a comment (single line)
+
+myString = '''
+This is a comment
+This is a comment
+This is a comment
+This is a comment
+This is a comment
+This is a comment
+'''
+
+# print(myString)
+
+"""
+this is another comment
+this is another comment
+this is another comment
+this is another comment
+"""
+
+# # strings
+
+# print("my name is ", name, "and i am ", age, "years old.")
+# print("my name is " +  str(name) + "and i am " + str(age) + "years old.")
+# print("my name is {name} and i am {age} years old.".format(name=name, age=age))
+
+# print(f"my name is {name} and i am {age} years old.")
+
+# # string methods
+x = "musah"
+# print(x.startswith("w"))
+
+# # taking input from users
+# name = float(input("Enter your name >> "))
+name = input("Enter your name >> ")
+print(type(name))
+
+# #  # conditionals
+# if, else, elif
+
+
